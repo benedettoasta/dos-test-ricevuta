adcbed632af1f5f940103cf3da2fd271d6de58db
benedetto.asta <benedetto.asta@gmail.com>
2020-03-07 12:05:18 +0100

    FoodTest:Test#2#3#4#5: after refactoring they automatic pass. Writed for future Refactor

--- src/test/com/dos/ricevuta/FoodTest.java
+++ src/test/com/dos/ricevuta/FoodTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
+import main.com.dos.ricevuta.products.Book;
 import main.com.dos.ricevuta.products.Food;
 
 class FoodTest {
@@ -14,5 +15,32 @@ class FoodTest {
 		Food food = new Food();
 		assertThat(food.getType(), Is.is("FOOD"));
 	}
-
+	
+	@Test
+	void givenFood_whenGetStringRicevuta_thenStringForRicevutaReturn() {
+		Food food = new Food("Cioccolata, calda",5,false);
+		String result = "Cioccolata, calda - FOOD: 5.00";
+		assertThat(food.getProductFormattedRicevutaString(), Is.is(result));
+	}
+	
+	@Test
+	void givenAnotherFood_whenGetStringRicevuta_thenStringForRicevutaReturn() {
+		Food food = new Food("Merenda al latte",20,false);
+		String result = "Merenda al latte - FOOD: 20.00";
+		assertThat(food.getProductFormattedRicevutaString(), Is.is(result));
+	}
+	
+	@Test
+	void givenImportedFood_whenGetStringRicevuta_thenStringForRicevutaReturn() {
+		Food food = new Food("Cioccolata, calda",5,true);
+		String result = "Cioccolata, calda - FOOD: 5.25";
+		assertThat(food.getProductFormattedRicevutaString(), Is.is(result));
+	}
+	
+	@Test
+	void givenAnotherImportedFood_whenGetStringRicevuta_thenStringForRicevutaReturn() {
+		Food food = new Food("Merenda al latte",20,true);
+		String result = "Merenda al latte - FOOD: 21.00";
+		assertThat(food.getProductFormattedRicevutaString(), Is.is(result));
+	}
 }
