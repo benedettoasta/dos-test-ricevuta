4b6dc8c1eb1410c2d97f92fbdc7ca2fbfd86c3d6
benedetto.asta <benedetto.asta@gmail.com>
2020-03-07 11:01:54 +0100

    FoodTest:Test#1: given Food return type FOOD

---
+++ src/main/com/dos/ricevuta/Food.java
@@ -0,0 +1,15 @@
+package main.com.dos.ricevuta;
+
+public class Food {
+
+	public Food()
+	{
+		
+	}
+
+	public String getType() {
+		// TODO Auto-generated method stub
+		return "FOOD";
+	}
+	
+}
---
+++ src/test/com/dos/ricevuta/FoodTest.java
@@ -0,0 +1,18 @@
+package test.com.dos.ricevuta;
+
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.Test;
+
+import main.com.dos.ricevuta.Food;
+
+class FoodTest {
+
+	@Test
+	void givenFood_whenGetType_thenFoodIsReturned() {
+		Food food = new Food();
+		assertThat(food.getType(), Is.is("FOOD"));
+	}
+
+}
