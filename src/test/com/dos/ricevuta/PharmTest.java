15b83e4f2df233f6a807e470b872bcae795422cc
benedetto.asta <benedetto.asta@gmail.com>
2020-03-07 12:30:15 +0100

    PharmTest:Test#2#3#4#5: pass with refactor. test added for next refactor check

--- src/test/com/dos/ricevuta/PharmTest.java
+++ src/test/com/dos/ricevuta/PharmTest.java
@@ -14,5 +14,33 @@ class PharmTest {
 		Pharm pharm = new Pharm();
 		assertThat(pharm.getType(), Is.is("PHARM"));
 	}
+	
+	@Test
+	void givenPharm_whenGetStringRicevuta_thenStringForRicevutaReturn() {
+		Pharm pharm = new Pharm("tachipirina, 10 - compresse",8,false);
+		String result = "tachipirina, 10 - compresse - PHARM: 8.00";
+		assertThat(pharm.getProductFormattedRicevutaString(), Is.is(result));
+	}
+	
+	@Test
+	void givenAnotherPharm_whenGetStringRicevuta_thenStringForRicevutaReturn() {
+		Pharm pharm = new Pharm("oki task",(float)12.5,false);
+		String result = "oki task - PHARM: 12.50";
+		assertThat(pharm.getProductFormattedRicevutaString(), Is.is(result));
+	}
+	
+	@Test
+	void givenImportedPharm_whenGetStringRicevuta_thenStringForRicevutaReturn() {
+		Pharm pharm = new Pharm("tachipirina, 10 - compresse",8,true);
+		String result = "tachipirina, 10 - compresse - PHARM: 8.40";
+		assertThat(pharm.getProductFormattedRicevutaString(), Is.is(result));
+	}
+	
+	@Test
+	void givenAnotherImportedPharm_whenGetStringRicevuta_thenStringForRicevutaReturn() {
+		Pharm pharm = new Pharm("oki task",(float)12.5,true);
+		String result = "oki task - PHARM: 13.13";
+		assertThat(pharm.getProductFormattedRicevutaString(), Is.is(result));
+	}
 
 }
