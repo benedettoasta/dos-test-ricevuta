2ae5c8807652a8c0fd1179b9010060ac2b2cc437
benedetto.asta <benedetto.asta@gmail.com>
2020-03-07 10:58:45 +0100

    remove unused imports

--- src/test/com/dos/ricevuta/BookTest.java
+++ src/test/com/dos/ricevuta/BookTest.java
@@ -1,8 +1,6 @@
 package test.com.dos.ricevuta;
 
 import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
