06b3ad2afa54d1f578f28851a33b3d37a1df025a
benedetto.asta <benedetto.asta@gmail.com>
2020-03-07 13:00:37 +0100

    MusicTest:Test#4#5#6#6: add test for product String for checking refactor

--- src/test/com/dos/ricevuta/MusicTest.java
+++ src/test/com/dos/ricevuta/MusicTest.java
@@ -6,6 +6,7 @@ import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
 import main.com.dos.ricevuta.products.Music;
+import main.com.dos.ricevuta.products.Music;
 
 class MusicTest {
 
@@ -31,6 +32,34 @@ class MusicTest {
 		float priceWithTax = 23;
 		Music music = new Music(descrizione,price,true);
 		assertThat(music.getPriceTaxed(), Is.is(priceWithTax));
+	}
+	
+	@Test
+	void givenMusic_whenGetStringRicevuta_thenStringForRicevutaReturn() {
+		Music music = new Music("Disco Innuendo",20,false);
+		String result = "Disco Innuendo - MUSIC: 22.00";
+		assertThat(music.getProductFormattedRicevutaString(), Is.is(result));
+	}
+	
+	@Test
+	void givenAnotherMusic_whenGetStringRicevuta_thenStringForRicevutaReturn() {
+		Music music = new Music("883 - Nord,sud,ovest,est",16,false);
+		String result = "883 - Nord,sud,ovest,est - MUSIC: 17.60";
+		assertThat(music.getProductFormattedRicevutaString(), Is.is(result));
+	}
+	
+	@Test
+	void givenImportedMusic_whenGetStringRicevuta_thenStringForRicevutaReturn() {
+		Music music = new Music("Disco Innuendo",20,true);
+		String result = "Disco Innuendo - MUSIC: 23.00";
+		assertThat(music.getProductFormattedRicevutaString(), Is.is(result));
+	}
+	
+	@Test
+	void givenAnotherImportedMusic_whenGetStringRicevuta_thenStringForRicevutaReturn() {
+		Music music = new Music("883 - Nord,sud,ovest,est",16,true);
+		String result = "883 - Nord,sud,ovest,est - MUSIC: 18.40";
+		assertThat(music.getProductFormattedRicevutaString(), Is.is(result));
 	}
 	
 }
