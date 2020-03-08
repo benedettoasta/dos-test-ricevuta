8d98fb806889840ac8d9781133a7025acb990948
benedetto.asta <benedetto.asta@gmail.com>
2020-03-08 14:34:02 +0100

    CartTest:Test#5: ten multiple bill have luckybonus string

--- src/main/com/dos/ricevuta/Cart.java
+++ src/main/com/dos/ricevuta/Cart.java
@@ -17,11 +17,14 @@ public class Cart {
 	protected static final String PRODUCT_SEPARATOR = " ";
 	protected static final String TOTAL_TAXES_LABEL = "TAXES: ";
 	protected static final String TOTAL_PRICE_LABEL = "TOTAL: ";
+	protected static final String LUCKY_BONUS_LABEL = "Se ti chiami ###NOME### hai vinto un buono da 50 euro";
 	
 	protected List<IProduct> products;
+	protected boolean luckyBonus;
 	
 	public Cart() {
 		this.products = new ArrayList<IProduct>();
+		this.luckyBonus = false;
 	}
 	
 	public int countItems() {
@@ -44,6 +47,15 @@ public class Cart {
 			cumulativeTaxes += product.getTaxes();
 		}
 		bill += TOTAL_TAXES_LABEL + ProductUtils.priceToString(cumulativeTaxes) + PRODUCT_SEPARATOR + TOTAL_PRICE_LABEL + ProductUtils.priceToString(cumulativePrices);
+		
+		if(luckyBonus) {
+			bill += PRODUCT_SEPARATOR + LUCKY_BONUS_LABEL;
+		}
 		return bill;
+	}
+
+	public void setLuckyBonus(boolean luckyBonus) {
+		// TODO Auto-generated method stub
+		this.luckyBonus = luckyBonus;
 	}
 }
--- src/test/com/dos/ricevuta/CartTest.java
+++ src/test/com/dos/ricevuta/CartTest.java
@@ -4,6 +4,8 @@
 package test.com.dos.ricevuta;
 
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
@@ -48,5 +50,24 @@ class CartTest {
 		
 		String resultExpected = "Il signore degli anelli - BOOK: 12.49 Innuendo - MUSIC: 16.49 Cioccolata - FOOD: 0.85 TAXES: 1.50 TOTAL: 29.83";
 		assertThat(cart.getBill(),Is.is(resultExpected));
+	}
+	
+	@Test
+	void givenCart_whenBillCountsIsTenMultiple_thenBillContainsLuckyBonus() {
+		Cart cart = new Cart();
+		cart.setLuckyBonus(true);
+		cart.addProduct(new Book("Il signore degli anelli",12.49F,false));
+		cart.addProduct(new Music("Innuendo",14.99F,false));
+		cart.addProduct(new Food("Cioccolata",0.85F,false));
+		
+		String startWithExpected = "Il signore degli anelli - BOOK: 12.49 Innuendo - MUSIC: 16.49 Cioccolata - FOOD: 0.85 TAXES: 1.50 TOTAL: 29.83";		
+		/**
+		 *  NOTA: Rimuovo "Se ti chiami ###NOME###" per evitare che il randomize sul nome faccia fallire i test.
+		 */
+		String containsExpected  = "hai vinto un buono da 50 euro";
+		
+		String bill = cart.getBill();
+		assertTrue(bill.startsWith(startWithExpected));
+		assertTrue(bill.contains(containsExpected));
 	}
 }
