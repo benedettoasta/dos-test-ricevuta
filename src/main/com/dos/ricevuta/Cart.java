646bf580423b8f1cdaead3f62ebbced40e781fb6
benedetto.asta <benedetto.asta@gmail.com>
2020-03-08 11:54:33 +0100

    CartTest:Test#2: count items return numbers of product in the cart

--- src/main/com/dos/ricevuta/Cart.java
+++ src/main/com/dos/ricevuta/Cart.java
@@ -3,17 +3,29 @@
  */
 package main.com.dos.ricevuta;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import main.com.dos.ricevuta.contracts.IProduct;
+
 /**
  * @author Benedetto
  *
  */
 public class Cart {
 	
+	protected List<IProduct> products;
+	
 	public Cart() {
-		
+		this.products = new ArrayList<IProduct>();
 	}
 	
 	public int countItems() {
-		return 0;
+		return this.products.size();
+	}
+
+	public void addProduct(IProduct product) {
+		// TODO Auto-generated method stub
+		this.products.add(product);
 	}
 }
--- src/test/com/dos/ricevuta/CartTest.java
+++ src/test/com/dos/ricevuta/CartTest.java
@@ -8,6 +8,8 @@ import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
 import main.com.dos.ricevuta.Cart;
+import main.com.dos.ricevuta.contracts.IProduct;
+import main.com.dos.ricevuta.products.Book;
 
 /**
  * @author Benedetto
@@ -19,6 +21,14 @@ class CartTest {
 	void givenNewCart_whenCountItems_thenCartIsEmpty() {
 		Cart cart = new Cart();
 		assertThat(cart.countItems(),Is.is(0));
+	}
+	
+	@Test
+	void givenNewCart_whenAddProduct_thenCountReturnNumberOfItems() {
+		Cart cart = new Cart();
+		IProduct product = new Book();
+		cart.addProduct(product);
+		assertThat(cart.countItems(),Is.is(1));
 	}
 
 }
